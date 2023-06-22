e7aa96f6ec6ae694d059c6755b4bfbd69980caa6
David Rebbe <drebbe@intrepidcs.com>
2023-06-22 19:24:10 -0400

    removed wrapper.h and use libicsneo source direct

--- build.rs
+++ build.rs
@@ -1,12 +1,26 @@
-#![windows_subsystem = "console"]
-use bindgen;
-use cmake;
-use path_clean::clean;
-use std::env;
-use std::path::PathBuf;
-use which;
+use cmake::Config;
+use path_clean::{clean, PathClean};
+use std::{env, path::PathBuf};
 
-#[cfg(feature = "build_libicsneo")]
+fn libicsneo_path() -> PathBuf {
+    // Get the path of libicsneo
+    let path = std::env::var("LIBICSNEO_PATH")
+        .unwrap_or(format!("{}/src/libicsneo", env!("CARGO_MANIFEST_DIR")));
+    let libicsneo_path = std::path::PathBuf::from(clean(&path));
+
+    libicsneo_path
+}
+
+fn libicsneo_include_path() -> PathBuf {
+    let path = libicsneo_path().join("include");
+    path.clean()
+}
+
+fn libicsneo_header_path() -> PathBuf {
+    let path = libicsneo_include_path().join("icsneo").join("icsneoc.h");
+    path.clean()
+}
+
 fn is_release_build() -> bool {
     let profile = std::env::var("PROFILE").unwrap();
     match profile.as_str() {
@@ -16,64 +30,10 @@ fn is_release_build() -> bool {
     }
 }
 
-fn _checkout_libicsneo_repo(libicsneo_path: &PathBuf) {
-    let output = std::process::Command::new("git")
-        .args(["clone", "git@github.com:intrepidcs/libicsneo.git", "."])
-        .current_dir(libicsneo_path)
-        .output()
-        .expect("Failed to fetch git submodules!");
-    // Make sure git was successful!
-    if !output.status.success() {
-        println!("cargo:warning=git return code: {}", output.status);
-        let stdout = std::str::from_utf8(&output.stdout).unwrap();
-        for line in stdout.split("\n") {
-            println!("cargo:warning=git stdout: {}", line);
-        }
-        let stderr = std::str::from_utf8(&output.stderr).unwrap();
-        for line in stderr.split("\n") {
-            println!("cargo:warning=git stderr: {}", line);
-        }
-        _checkout_libicsneo_repo(&libicsneo_path);
-        panic!("Failed to clone libicsneo github repo!");
-    }
-}
-
-fn _prepare_git_submodule(libicsneo_path: &PathBuf) {
-    // This seems to not be needed when including this as a dependency? Why?
-    // checkout the submodule if needed
-    let output = std::process::Command::new("git")
-        .args(["submodule", "update", "--init"])
-        .current_dir(libicsneo_path)
-        .output()
-        .expect("Failed to fetch git submodules!");
-    // Make sure git was successful!
-    if !output.status.success() {
-        println!("cargo:warning=git return code: {}", output.status);
-        let stdout = std::str::from_utf8(&output.stdout).unwrap();
-        for line in stdout.split("\n") {
-            println!("cargo:warning=git stdout: {}", line);
-        }
-        let stderr = std::str::from_utf8(&output.stderr).unwrap();
-        for line in stderr.split("\n") {
-            println!("cargo:warning=git stderr: {}", line);
-        
-        }
-        // If we are a dependency in another crate we aren't a git repo anymore.
-        _checkout_libicsneo_repo(&libicsneo_path);
-    }
-}
-
-#[cfg(feature = "build_libicsneo")]
-fn build_libicsneo(libicsneo_path: &PathBuf) {
-    use cmake::Config;
-
+fn build_libicsneo() {
+    let libicsneo_path = libicsneo_path();
     // Check to make sure CMakeLists.txt exists
-    if libicsneo_path.join("CMakeLists.txt").exists() {
-        println!(
-            "cargo:warning=Found CMakeLists.txt at: {}",
-            libicsneo_path.display()
-        );
-    } else {
+    if !libicsneo_path.join("CMakeLists.txt").exists() {
         panic!("CMakeLists.txt not found at {}", libicsneo_path.display());
     }
     let build_config_type = if cfg!(target_os = "windows") {
@@ -92,21 +52,16 @@ fn build_libicsneo(libicsneo_path: &PathBuf) {
     // Run cmake on libicsneo
     let mut config = Config::new(libicsneo_path.clone());
     #[allow(unused_mut)]
-    let mut config = config.build_target("ALL_BUILD")
+    let mut config = config
+        .build_target("ALL_BUILD")
         .define("LIBICSNEO_BUILD_ICSNEOC_STATIC:BOOL", "ON")
         .define("LIBICSNEO_BUILD_EXAMPLES:BOOL", "OFF")
         .define("LIBICSNEO_BUILD_ICSNEOLEGACY:BOOL", "OFF")
-        .profile(build_config_type); 
+        .profile(build_config_type);
     // Lets use ninja if the feature is enabled
-    let config = if cfg!(feature = "ninja") {
-        // Check to make sure ninja exists on this platform's path
-        match which::which("ninja") {
-            Ok(_) => {},
-            Err(e) => panic!("Failed to find ninja executable in system path. Is it installed? (Error: {:#?})", e),
-        };
-        config.generator("Ninja Multi-Config").build_target("all")
-    } else {
-        config
+    let config = match which::which("ninja") {
+        Ok(_) => config.generator("Ninja Multi-Config").build_target("all"),
+        Err(_e) => config,
     };
     let dst = config.build();
     // output for lib path
@@ -114,11 +69,21 @@ fn build_libicsneo(libicsneo_path: &PathBuf) {
         "cargo:warning=icsneoc.lib search path: {:?}",
         dst.join(format!("build/{build_config_type}")).display()
     );
-    // Configure the search path and lib name to link to for icsneoc-static and icsneocpp
+    // icsneo lib/dll linker search path
     println!(
         "cargo:rustc-link-search=native={}",
         dst.join(format!("build/{build_config_type}")).display()
     );
+    // fatfs linker search path and addition
+    println!(
+        "cargo:rustc-link-search=native={}/build/third-party/fatfs/{build_config_type}",
+        dst.display()
+    );
+    println!("cargo:rustc-link-lib=fatfs");
+    // icsneocpp linker addition
+    println!("cargo:rustc-link-lib=icsneocpp");
+
+    /*
     println!("cargo:rustc-link-lib=static=icsneoc-static");
     println!("cargo:rustc-link-lib=icsneocpp");
     // Configure the search path and lib name to link to for fatfs
@@ -128,18 +93,60 @@ fn build_libicsneo(libicsneo_path: &PathBuf) {
         "cargo:rustc-link-search=native={}/build/third-party/fatfs/{build_config_type}", dst.display()
     );
     println!("cargo:rustc-link-lib=fatfs");
+    */
 }
 
-#[cfg(not(feature = "build_libicsneo"))]
-fn build_libicsneo(_: &PathBuf) {}
+#[cfg(not(feature = "static"))]
+fn setup_linker_libs() {
+    println!("cargo:rustc-link-lib=dylib=icsneoc");
+    match env::var("CARGO_CFG_TARGET_OS").unwrap().as_str() {
+        "macos" => {
+            println!("cargo:rustc-link-lib=framework=IOKit");
+            println!("cargo:rustc-link-lib=framework=CoreFoundation");
+        }
+        "linux" | "windows" => {}
+        target_os => panic!("Target OS not supported: {target_os}"),
+    }
+}
 
-fn generate_bindings(libicsneo_path: &PathBuf) {
-    let include_path = libicsneo_path.join("include");
-    println!("cargo:warning=icsneo include path: {:?}", include_path);
-    println!("cargo:warning={}", include_path.display());
+#[cfg(feature = "static")]
+fn setup_linker_libs() {
+    match env::var("CARGO_CFG_TARGET_OS").unwrap().as_str() {
+        "windows" => {
+            println!("cargo:rustc-link-lib=static=icsneoc-static");
+        }
+        "linux" => {
+            println!("cargo:rustc-link-lib=static=icsneoc-static");
+        }
+        "macos" => {
+            println!("cargo:rustc-link-lib=static=icsneoc-static");
+            println!("cargo:rustc-link-lib=framework=IOKit");
+            println!("cargo:rustc-link-lib=framework=CoreFoundation");
+        }
+        target_os => panic!("Target OS not supported: {target_os}"),
+    }
+}
+
+fn generate_bindings() {
+    let header = libicsneo_header_path();
     let bindings = bindgen::Builder::default()
-        .header("wrapper.h")
-        .clang_arg(format!("-I{}", include_path.display()))
+        .header(header.to_str().unwrap())
+        .default_enum_style(bindgen::EnumVariation::Rust {
+            non_exhaustive: false,
+        })
+        .clang_args(&[format!("-I{}", libicsneo_include_path().display()).as_str()])
+        .allowlist_function("icsneo_.*")
+        .allowlist_type("neodevice_t")
+        .allowlist_type("neonetid_t")
+        //.allowlist_type("neomessage_.*")
+        .allowlist_type("neoversion_t")
+        .allowlist_type("neoevent_t")
+        //.formatter(bindgen::Formatter::Rustfmt)
+        .derive_default(true)
+        .derive_debug(true)
+        //.derive_partialeq(true)
+        .parse_callbacks(Box::new(bindgen::CargoCallbacks))
+        //.clang_args(clang_args())
         .generate()
         .expect("Unable to generate bindings");
     let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
@@ -147,23 +154,20 @@ fn generate_bindings(libicsneo_path: &PathBuf) {
     bindings
         .write_to_file(out_path.join("bindings.rs"))
         .expect("Couldn't write bindings");
+
+    let out_path = std::path::PathBuf::from(env::var("OUT_DIR").unwrap());
+    bindings
+        .write_to_file(out_path.join("bindings.rs"))
+        .expect("Couldn't write bindings!");
 }
 
 fn main() {
-    // Get the path of libicsneo
-    let path = std::env::var("LIBICSNEO_PATH")
-        .unwrap_or(format!("{}/src/libicsneo", env!("CARGO_MANIFEST_DIR")));
-    let libicsneo_path = std::path::PathBuf::from(clean(&path));
+    let header = libicsneo_header_path();
+    println!("cargo:rerun-if-changed=build.rs");
+    println!("cargo:rerun-if-changed={}", header.to_str().unwrap());
+    println!("cargo:rerun-if-env-changed=LIBMSVC_PATH");
 
-    if std::env::var("DOCS_RS").is_err() {
-        /*
-        // We don't need to checkout the submodule if we are using a custom libicsneo path
-        if std::env::var("LIBICSNEO_PATH").is_err() {
-            _prepare_git_submodule(&libicsneo_path);
-        }
-        */
-        build_libicsneo(&libicsneo_path);
-    }
-    // lets generate the bindings
-    generate_bindings(&libicsneo_path);
+    generate_bindings();
+    build_libicsneo();
+    setup_linker_libs();
 }
